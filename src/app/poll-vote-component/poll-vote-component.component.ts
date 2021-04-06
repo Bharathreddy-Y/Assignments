22bf8338ab1484ee17b19d53b326be3458037b26
Bharathreddy-Y <bharathkumar.yedla@archents.com>
2021-04-06 17:30:34 +0530

    added filter functionality and one can vote who is above 18.

--- src/app/poll-vote-component/poll-vote-component.component.ts
+++ src/app/poll-vote-component/poll-vote-component.component.ts
@@ -12,19 +12,76 @@ export class PollVoteComponentComponent implements OnInit {
     {id:1,name:"Chandra Babu Thatha",imgSrc:"/../../assets/Voters Images/cycle.jfif",count:0},
     {id:2,name:"K.A Paul Uncle",imgSrc:"/../../assets/Voters Images/helicopter.jfif",count:0}
   ]
+  public votersArray=[
+    {id:0,name:"Bharath",age:26,isVote:false,votecount:0},
+    {id:1,name:"Arun",age:18,isVote:false,votecount:0},
+    {id:2,name:"Pawan",age:23,isVote:false,votecount:0},
+    {id:3,name:"Ram",age:22,isVote:false,votecount:0},
+    {id:4,name:"Harish",age:26,isVote:false,votecount:0},
+    {id:5,name:"Sravani",age:15,isVote:false,votecount:0},
+  ]
+  public voterAge:number=0
+  public selectedVoter:string=""
+  public message:string=""
+  public votermessage:string=""
+  public showMessage:boolean=false
+  public showBallotBox:boolean=false
+  public showVoters:boolean=true
+  public voterExistMessage:boolean=false
+  public totalVotes:number=0
+  public isVoted:boolean=false
   constructor() { }
 
   ngOnInit(): void {
   }
+  onClickToSelectVoter(event:any){
+    // console.log(event)
+    this.votersArray.forEach((voter:any)=>{
+      if(voter.name === event)
+      {
+        if(voter.age > 18)
+        {
+          console.log(voter.votecount)
+          voter.votecount += 1
+          if(voter.votecount<=1)
+          {
+            this.showVoters=false
+            this.showBallotBox=true
+            this.voterExistMessage=false
+            this.showMessage=false
+          }
+          else{
+            this.showVoters=true
+            this.showBallotBox=false
+            voter.isVote = false
+            this.isVoted = voter.isVote
+            this.voterExistMessage=true
+            this.showMessage=false
+            this.votermessage = " You Have already Resigtered Your Vote"
+          }
+        }
+        else{
+          this.showMessage=true
+          this.showBallotBox=false
+          this.voterExistMessage=false
+          this.showVoters = true
+          this.isVoted=voter.isVote
+          this.message="You are not Eligible to Vote."
+        }
+      }
+    })
+  }
   onClickToVote(id:any){
-    console.log("hi from",id)
+    this.totalVotes+=1
     this.candidateArray.forEach((item:any)=>
     {
       if(item.id === id)
       {
+        this.showBallotBox=false
+        this.showVoters=true
         item.count += 1
       }
-      console.log("i am "+item.name+" my count is "+item.count)
     })
+    // console.log("Total votes "+this.totalVotes)
   }
 }
